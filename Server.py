8410420dca29a0d7534b82d8fa9c7cc98530a011
Drareeg <geerard.ponney@gmail.com>
2014-05-30 03:10:46 +0200

    some game add

--- Server.py
+++ Server.py
@@ -3,7 +3,6 @@ import tornado.web
 import tornado.websocket
 import game
 import json
-import Encoder
 import random
 import logging
 #import pdb;pdb.set_trace()
@@ -24,13 +23,13 @@ def send(client, message):
 def handlemessage(originclient, message):
     splat = message.split(" ");
     if splat[0] == "CREATEROOM":
-        room = game.Room(random.randint(1, 1))  #logica achter het gekozen id steken
-        rooms[room.id] = room
+        room = Room(random.randint(1, 1))  #logica achter het gekozen id steken
+        rooms[room.roomId] = room
     if splat[0] == "GETROOMS":
-        send(originclient, json.dumps(rooms, cls=Encoder.CustomEncoder, indent=2))
+        send(originclient, json.dumps(rooms, cls=MyEncoder, indent=2))
     if splat[0] == "JOINROOM":  #JOINROOM roomid playername
         room = rooms.get(int(splat[1]))  #handle null
-        if room.join(game.WebPlayer(splat[2])):
+        if room.join(game.Player(splat[2])):
             send(originclient, "you joined room ")
 
 
@@ -48,6 +47,35 @@ class MessageHandler(tornado.websocket.WebSocketHandler):
     def on_close(self):
         logging.info("someone closed")
         clients.remove(self)
+
+
+class Room:  #hehe he
+
+
+
+    def __init__(self, id):
+        logging.info("room made")
+        self.roomId = id
+        self.players = list()
+
+
+    def join(self, player):
+        logging.info("%s tries joining room" % player.name)
+        self.players.append(player)
+        return 1  #if succes, currently always accepting players
+
+
+    def to_json(self):
+        return json.dumps(self.roomId) + "," + json.dumps(self.players.__len__())
+
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        print(obj)
+        if isinstance(obj, Room):
+            return obj.to_json()
+
+        return json.JSONEncoder.default(self, obj)
 
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
--- game.py
+++ game.py
@@ -1,36 +1,83 @@
 __author__ = 'Drareeg'
-import json
 import logging
+import random
 
 
-class Room:  #hehe he
-    players = list()
-    id = int
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 
-    def __init__(self, id):
-        logging.info("room made")
-        self.id = id
+# the abstract object representing the game
+class Game:
+    def __init__(self):
+        self.players = ()
+        self.started = False
+        self.turns = 10
+        self.current_turn = 0
+
+    def join_game(self, player):
+        if not self.started:
+            self.players.append(player)
+            player.set_game(self)
+        else:
+            raise Exception("Cant join a started game")
+
+    def deal(self):
+        self.started = True
+        deck = Deck()
+        for i in range(self.turns):
+            for player in self.players:
+                player.add_card(deck.get_next_card())
+
+    def check_all_players_selected(self):
+        for player in self.players:
+            if player.selected_card is None:
+                return False
+        return True
 
 
-    def join(self, player):
-        logging.info("%s tries joining room" % player.name)
-        self.players.append(player)
-        return 1  #if succes, currently always accepting players
 
 
-    def to_json(self):
-        return json.dumps(self.id) + "," + json.dumps(self.players.__len__())
-
-
-class WebPlayer:
+class Player:
     def __init__(self, name):
         self.name = name
+        self.cards = ()
+        self.selected_card = None
+        self.game = None
 
+    def add_card(self, card):
+        self.cards.append(card)
 
     def to_json(self):
         return self.name
 
+    def set_game(self, game):
+        self.game = game
+
+    #the x-th card in hand
+    def select_card(self, card):
+        if card < len(self.cards):
+            self.selected_card = self.cards(i)
 
 
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+class Card:
+    def __init__(self, value, points):
+        self.value = value
+        self.points = points
+
+
+class Deck:
+    def __init__(self):
+        self.cards = ()
+        for i in range(104):
+            self.cards.append(Card(i, 1))
+
+        random.shuffle(self.cards)
+
+
+    def get_next_card(self):
+        return self.cards.pop()
+
+
+
+
+
